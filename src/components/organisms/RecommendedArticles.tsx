8c6fd20d63774990f658619d2856518cf8c3c75d
yumekiti <yumekiti120440@gmail.com>
2024-06-25 02:57:57 +0900

    ✨ Implemented fetching recommended_articles data.

--- src/components/organisms/RecommendedArticles.tsx
+++ src/components/organisms/RecommendedArticles.tsx
@@ -3,7 +3,11 @@ import type { FC } from "react";
 import { Link } from "react-router-dom";
 import useSWR from "swr";
 
-import { recommended_articles } from "@/constants/articles";
+interface Article {
+	id: number;
+	title: string;
+	link: string;
+}
 
 const Component: FC = () => {
 	const { data, error, isLoading } = useSWR(
@@ -13,7 +17,20 @@ const Component: FC = () => {
 	if (error) return "notfound";
 	if (isLoading) return "loading";
 
-	console.log(data.page.revision.body);
+	const recommended_articles: Article[] = data.page.revision.body
+		.split(/\n/)
+		.map((line: string, index: number) => {
+			const regex = /\[(.+?)\]\((.+?)\)/;
+			const match = line.match(regex);
+			if (match) {
+				const id = index;
+				const title = match[1];
+				const link = match[2].match(/[^/]+$/)?.[0];
+				return { id, title, link };
+			}
+			return { id: 0, title: "", link: "" };
+		})
+		.filter((article: Article) => article.title !== "" && article.link !== "");
 
 	return (
 		<div className="py-2">
@@ -25,10 +42,10 @@ const Component: FC = () => {
 					</h3>
 				</div>
 				<ol className="w-3/4 mx-auto list-decimal space-y-4">
-					{recommended_articles.map((article) => (
+					{recommended_articles.map((article: Article) => (
 						<li key={article.id}>
 							<Link
-								to={`/contents/${article.id}`}
+								to={`/contents/${encodeURIComponent(article.link)}`}
 								className="break-all line-clamp-5 hover:text-[#3da9fc] hover:underline"
 							>
 								{article.title}
